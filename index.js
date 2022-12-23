2714bc96ff42e2c2e5e54fc38473138259487bf4
JoseAugustoDev <75041668+JoseAugustoDev@users.noreply.github.com>
2022-12-23 18:19:35 -0300

    #

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const tabela = document.querySelector('.info')
+const cliente = document.querySelector('#nomeCliente')
+const endereco = document.querySelector('#endereco')
 const produto = document.querySelector('.productSelect')
 const valor = document.querySelector('.priceValue')
 const form = document.querySelector('#form')
@@ -7,19 +9,23 @@ function addVenda(event) {
     event.preventDefault();
 
     let product = produto.value
+    let client = cliente.value
+    let addres = endereco.value
     let value = parseInt(valor.value)
 
     tabela.style.display = 'flex'
     tabela.innerHTML = '<h3>Venda adicionada com sucesso!</h3>'
     
     class venda {
-        constructor(produto = 'undefined', valor = 0) {
+        constructor(nomeCliente = 'undefined', endereco = 'undefined', produto = 'undefined', valor = 0) {
+            this.cliente = client
+            this.endereco = addres
             this.produto = produto
             this.valor = valor
         }
     }
 
-    const vendaFinal = new venda(product, value)
+    const vendaFinal = new venda(nomeCliente, endereco, product, value)
     console.log(vendaFinal)
 }
 
